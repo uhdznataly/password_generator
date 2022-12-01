4f0f3594b1b59e0b27b1deb32307e24232c3db61
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2022-12-01 17:30:05 -0600

    declare variables

--- script.js
+++ script.js
@@ -4,9 +4,30 @@ var generateBtn = document.querySelector("#generate");
 
 // Create Arrays to use for password generator
 var number = ["0","1","2","3","4","5","6","7","8","9"];
-var character = ["!","@","#","$","%","^","&","*","?"];
+var specialCharacter = ["!","@","#","$","%","^","&","*","?"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Declare variables 
+ var confirmLength = "";
+ var confirmSpecialCharacter;
+ var confirmNumber;
+ var confirmUpper;
+ var confirmLower;
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
