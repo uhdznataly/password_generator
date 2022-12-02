b37a5f31b03f5c352a396de73c3edeb674ea4960
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2022-12-01 18:07:40 -0600

    add event listener

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Create Arrays to use for password generator
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
